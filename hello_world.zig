0a84da8905401deb1dc300040201865efbec141f
Trevor Apple <trappleco@gmail.com>
2026-03-30 07:28:51 -0700

    working on sdl3 hello world

--- hello_world.zig
+++ hello_world.zig
@@ -1,23 +1,42 @@
+const sdl3 = @import("sdl3");
 const std = @import("std");
-const print = std.debug.print;
 
-pub fn rot(txt: []u8, key: u8) void {
-    for (txt, 0..txt.len) |c, i| {
-        if (std.ascii.isLower(c)) {
-            txt[i] = (c - 'a' + key) % 26 + 'a';
-        } else if (std.ascii.isUpper(c)) {
-            txt[i] = (c - 'A' + key) % 26 + 'A';
-        }
-    }
-}
+const fps = 60;
+const screen_width = 640;
+const screen_height = 480;
 
 pub fn main() !void {
-    const key = 3;
-    var txt = "The five boxing wizards jump quickly".*;
+    defer sdl3.shutdown();
 
-    print("Original:  {s}\n", .{txt});
-    rot(&txt, key);
-    print("Encrypted: {s}\n", .{txt});
-    rot(&txt, 26 - key);
-    print("Decrypted: {s}\n", .{txt});
+    // Initialize SDL with subsystems you need here.
+    const init_flags = sdl3.InitFlags{ .video = true };
+    try sdl3.init(init_flags);
+    defer sdl3.quit(init_flags);
+
+    // Initial window setup
+    const window = try sdl3.video.Window.init("Hello SDL3", screen_width, screen_height, .{});
+    defer window.deinit();
+
+    // Useful for limiting the FPS and getting the delta time
+    var fps_capper = sdl3.extras.FramerateCapper(f32){ .mode = .{ .limited = fps } };
+
+    var quit = false;
+    while (!quit) {
+        // Delay to limit the FPS, returned delta time not needed.
+        const dt = fps_capper.delay();
+        _ = dt;
+
+        // Update logic
+        const surface = try window.getSurface();
+        try surface.fillRect(null, surface.mapRgb(128, 30, 255));
+        try window.updateSurface();
+
+        // Event logic
+        while (sdl3.events.poll()) |event|
+            switch (event) {
+                .quit => quit = true,
+                .terminating => quit = true,
+                else => {},
+            };
+    }
 }
